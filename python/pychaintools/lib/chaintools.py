e3142972927acf0a61c3fd7f5860f4d47fdea6da
aadya940 <aadya@unify.ai>
2024-06-30 14:50:56 +0530

    format

--- python/pychaintools/lib/chaintools.py
+++ python/pychaintools/lib/chaintools.py
@@ -1,5 +1,6 @@
 import chain_lib
 
+
 def build_matrix_from_file(fname):
     _arr = chain_lib._create_chain_from_file(fname)
     return _arr
--- python/pychaintools/setup.py
+++ python/pychaintools/setup.py
@@ -7,11 +7,16 @@ extension_mod = Extension(
         "lib/chain_lib.c",
         "../../src/chaintools.c",
         "../../src/utils.c",
-    ],                                                                                      # List of source files
-    include_dirs=["../../src", "../../include", np.get_include(), "/usr/include/gsl"],      # Include directories
-    libraries=["chaintools", "gsl", "gslcblas"],                                            # Link against libchaintools.a
-    library_dirs=["../../", "/usr/lib"],                                                    # Directory containing libchaintools.a
-    extra_compile_args=["-std=c11"],                                                        # Optional: specify C standard
+    ],  # List of source files
+    include_dirs=[
+        "../../src",
+        "../../include",
+        np.get_include(),
+        "/usr/include/gsl",
+    ],  # Include directories
+    libraries=["chaintools", "gsl", "gslcblas"],  # Link against libchaintools.a
+    library_dirs=["../../", "/usr/lib"],  # Directory containing libchaintools.a
+    extra_compile_args=["-std=c11"],  # Optional: specify C standard
 )
 
 setup(
@@ -19,5 +24,5 @@ setup(
     version="1.0",
     description="Python extension module for Markov chain operations",
     ext_modules=[extension_mod],
-    install_requires=["numpy"],                                                             # Ensure numpy is installed
+    install_requires=["numpy"],  # Ensure numpy is installed
 )
